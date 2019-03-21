8e02b488d7e84ba3a1a339dacba606365622c61c
Mathe Emese <emese.mathe.07@gmail.com>
2019-03-21 18:06:44 +0200

    Binded a key A to a button

---
+++ Assets/Scripts/BindKeyToButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BindKeyToButton : MonoBehaviour {
+
+	public KeyCode _keyCode;
+	public Button _buttonToBind; 
+	// Use this for initialization
+	void Awake()
+	{
+		_buttonToBind = GetComponent<Button> ();
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (_keyCode)) {
+			_buttonToBind.onClick.Invoke ();
+		} else if (Input.GetKeyUp (_keyCode)) {
+			FadeColor ();
+		}
+			
+	}
+
+	void FadeColor()
+	{
+		Graphic graphic = GetComponent<Graphic> ();
+		graphic.CrossFadeColor (Color.black, _buttonToBind.colors.fadeDuration, true, true);
+	}
+}
---
+++ Assets/Scripts/Dictionary.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dictionary : MonoBehaviour {
+
+	private List<string> _content;
+	public InputField _input;
+	public Button _button;
+	public Dropdown _suggestions;
+	// Use this for initialization
+	void Start () {
+		_content = new List<string> ();
+		_content.Add ("dummy");
+		_content.Add ("hello");
+		_content.Add ("world");
+		_content.Add ("foo");
+		_content.Add ("bar");
+
+		_button = GetComponent<Button> ();
+		_button.onClick.AddListener(OnClickTask);
+
+		_input = GameObject.Find ("SearchWord").GetComponent<InputField> ();
+	}
+
+	void OnClickTask()
+	{
+		string word = _input.text;
+		List<string> possibileMatches = findSuggestions (word);
+		Debug.Log ("1." + possibileMatches [0]);
+		handleUIElements (possibileMatches);
+	}
+
+	void handleUIElements(List<string> dropDown)
+	{
+		if (dropDown.Count <= 0) {
+			return;
+		}
+
+		Debug.Log ("UI");
+		_suggestions.enabled = true;
+		_suggestions.ClearOptions ();
+		_suggestions.AddOptions (dropDown);
+		_suggestions.RefreshShownValue ();
+		_suggestions.Show ();
+	}
+		
+
+	public List<string> findSuggestions(string word)
+	{
+		if (word == null) {
+			Debug.Log ("empty");
+			return null;
+		}
+		List<string> possibleMatches = new List<string> ();
+		int length = word.Length;
+		foreach (string currentWord in _content) {
+			if (currentWord.Substring(0,length).Equals(word))
+			{
+				Debug.Log ("found something");
+				possibleMatches.Add(currentWord);
+			}
+		}
+		return possibleMatches;
+	}
+
+}
